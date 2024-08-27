9623b05002c53a901ed4a50468a5a96417ea39b7
spinner99-bit <121241376+spinner99-bit@users.noreply.github.com>
2024-08-28 03:00:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,59 +2,391 @@ let products = {
     data: [
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     {
-        productName: "M0006",
+        productName: "MISS0050",
+        category: "自慰",
+        image: "Product/MISS0050.jpg",
+    },
+
+
+    {
+        productName: "MISS0049",
+        category: "中国妹",
+        image: "Product/MISS0049.jpg",
+    },
+
+
+    {
+        productName: "MISS0048",
+        category: "自慰",
+        image: "Product/MISS0048.jpg",
+    },
+
+
+    {
+        productName: "MISS0047",
+        category: "校服",
+        image: "Product/MISS0047.jpg",
+    },
+
+
+    {
+        productName: "MISS0046",
+        category: "偷拍",
+        image: "Product/MISS0046.jpg",
+    },
+
+
+    {
+        productName: "MISS0045",
+        category: "泰国妹",
+        image: "Product/MISS0045.jpg",
+    },
+
+
+    {
+        productName: "MISS0044",
+        category: "新加坡妹",
+        image: "Product/MISS0044.jpg",
+    },
+
+
+    {
+        productName: "MISS0043",
+        category: "口交",
+        image: "Product/MISS0043.jpg",
+    },
+
+
+    {
+        productName: "MISS0042",
+        category: "做爱",
+        image: "Product/MISS0042.jpg",
+    },
+
+
+    {
+        productName: "MISS0041",
+        category: "自慰",
+        image: "Product/MISS0041.jpg",
+    },
+
+
+    {
+        productName: "MISS0040",
+        category: "自慰",
+        image: "Product/MISS0040.jpg",
+    },
+
+
+    {
+        productName: "MISS0039",
+        category: "自慰",
+        image: "Product/MISS0039.jpg",
+    },
+
+
+    {
+        productName: "MISS0038",
+        category: "自慰",
+        image: "Product/MISS0038.jpg",
+    },
+
+
+    {
+        productName: "MISS0037",
+        category: "泰国妹",
+        image: "Product/MISS0037.jpg",
+    },
+
+
+    {
+        productName: "MISS0036",
+        category: "口交",
+        image: "Product/MISS0036.jpg",
+    },
+
+
+    {
+        productName: "MISS0035",
         category: "自拍",
-        image: "1.jpg",
+        image: "Product/MISS0035.jpg",
     },
 
+
     {
-        productName: "M0005",
+        productName: "MISS0034",
+        category: "幼女",
+        image: "Product/MISS0034.jpg",
+    },
+
+
+    {
+        productName: "MISS0033",
+        category: "自拍",
+        image: "Product/MISS0033.jpg",
+    },
+
+
+    {
+        productName: "MISS0032",
+        category: "自拍",
+        image: "Product/MISS0032.jpg",
+    },
+
+
+    {
+        productName: "MISS0031",
+        category: "自拍",
+        image: "Product/MISS0031.jpg",
+    },
+
+
+    {
+        productName: "MISS0030",
+        category: "自拍",
+        image: "Product/MISS0030.jpg",
+    },
+
+
+    {
+        productName: "MISS0029",
+        category: "口交",
+        image: "Product/MISS0029.jpg",
+    },
+
+
+    {
+        productName: "MISS0028",
+        category: "网红",
+        image: "Product/MISS0028.jpg",
+    },
+
+
+    {
+        productName: "MISS0027",
+        category: "网红",
+        image: "Product/MISS0027.jpg",
+    },
+
+
+    {
+        productName: "MISS0026",
+        category: "网红",
+        image: "Product/MISS0026.jpg",
+    },
+
+
+    {
+        productName: "MISS0025",
         category: "自慰",
-        image: "2.jpg",
+        image: "Product/MISS0025.jpg",
     },
 
-    {
-        productName: "M0004",
-        category: "口交",
-        image: "3.jpg",
-    },
 
     {
-        productName: "M0003",
-        category: "口交",
-        image: "4.jpg",
+        productName: "MISS0024",
+        category: "同性",
+        image: "Product/MISS0024.jpg",
     },
 
-    {
-        productName: "M0002",
-        category: "口交",
-        image: "5.jpg",
-    },
 
     {
-        productName: "M0001",
+        productName: "MISS0023",
         category: "自慰",
-        image: "6.jpg",
+        image: "Product/MISS0023.jpg",
+    },
+
+
+    {
+        productName: "MISS0022",
+        category: "自慰",
+        image: "Product/MISS0022.jpg",
+    },
+
+
+    {
+        productName: "MISS0021",
+        category: "网红",
+        image: "Product/MISS0021.jpg",
+    },
+
+
+    {
+        productName: "MISS0020",
+        category: "自拍",
+        image: "Product/MISS0020.jpg",
+    },
+
+
+    {
+        productName: "MISS0019",
+        category: "自拍",
+        image: "Product/MISS0019.jpg",
+    },
+
+
+    {
+        productName: "MISS0018",
+        category: "自拍",
+        image: "Product/MISS0018.jpg",
+    },
+
+
+    {
+        productName: "MISS0017",
+        category: "网红",
+        image: "Product/MISS0017.jpg",
+    },
+
+
+    {
+        productName: "MISS0016",
+        category: "极品",
+        image: "Product/MISS0016.jpg",
+    },
+
+
+    {
+        productName: "MISS0015",
+        category: "泰国妹",
+        image: "Product/MISS0015.jpg",
+    },
+
+
+    {
+        productName: "MISS0014",
+        category: "自慰",
+        image: "Product/MISS0014.jpg",
+    },
+
+
+    {
+        productName: "MISS0013",
+        category: "自慰",
+        image: "Product/MISS0013.jpg",
+    },
+
+
+    {
+        productName: "MISS0012",
+        category: "自拍",
+        image: "Product/MISS0012.jpg",
+    },
+
+
+    {
+        productName: "MISS0011",
+        category: "自拍",
+        image: "Product/MISS0011.jpg",
+    },
+
+
+    {
+        productName: "MISS0010",
+        category: "做爱",
+        image: "Product/MISS0010.jpg",
+    },
+
+
+    {
+        productName: "MISS0009",
+        category: "自慰",
+        image: "Product/MISS0009.jpg",
+    },
+
+    {
+        productName: "MISS0008",
+        category: "自慰",
+        image: "Product/MISS0008.jpg",
+    },
+
+    {
+        productName: "MISS0007",
+        category: "自慰",
+        image: "Product/MISS0007.jpg",
+    },
+
+    {
+        productName: "MISS0006",
+        category: "自慰",
+        image: "Product/MISS0006.jpg",
+    },
+
+    {
+        productName: "MISS0005",
+        category: "自拍",
+        image: "Product/MISS0005.jpg",
+    },
+
+    {
+        productName: "MISS0004",
+        category: "自拍",
+        image: "Product/MISS0004.jpg",
+    },
+
+    {
+        productName: "MISS0003",
+        category: "姐弟",
+        image: "Product/MISS0003.jpg",
+    },
+
+    {
+        productName: "MISS0002",
+        category: "校服",
+        image: "Product/MISS0002.jpg",
+    },
+
+    {
+        productName: "MISS0001",
+        category: "校服",
+        image: "Product/MISS0001.jpg",
     },
 
 
     {
         productName: "一次购买 3 送 2 ( 自选 )",
         category: "活动",
-        image: "Promotion.jpg",
+        image: "Product/Promotion.jpg",
     },
 
     {
         productName: "一次购买 5 送 3 ( 自选 )",
         category: "活动",
-        image: "Promotion.jpg",
+        image: "Product/Promotion.jpg",
     },
 
     {
         productName: "一次购买 10 送 5 ( 自选 )",
         category: "活动",
-        image: "Promotion.jpg",
+        image: "Product/Promotion.jpg",
     },
 
 
